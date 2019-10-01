8916842439242021ae0f0f849430fc7fed9a8c53
Ian F. Smith <ian.smith@slayer.com>
2019-10-01 13:49:45 +0100

    Updates to contract and deploy_contract script.

---
+++ bin/deploy_contract_01.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#******************************************************************************
+#* Filename:            deploy_contract_01.py
+#* Last Updated:        2019-09-26
+#* Status:              Prototype
+#* Description:         Basic script to assist in contract deployment
+#* Project:             Zilliqa dApp Project
+#******************************************************************************
+#*     Copyright (c) 2019 Well Bred Software Limited (on behalf of DX3 Core)
+#*                           All rights reserved
+#******************************************************************************
+
+#  cd 
+
+__TOOL = "deploy_contract_01.py"
+__VERSION = "0.2.3-b26"
+__DESC = "Refactored into main(); Added payee_account setup in init params"
+
+import sys
+import time
+import src.zil_01 as zil_01
+from pyzil.contract import Contract
+from pyzil.account import Account
+
+
+filename = "src/me_tmp_01.scilla"
+filename = "src/me_ex1_oob_loc.scilla"
+
+def main(argv):
+    global filename
+
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    
+    start = time.time()
+    print(f"Starting at: {time.ctime(start)}")
+    
+    zil = zil_01.Zil_01(account=None)
+    
+    print("Account address:", zil.account.address)
+    
+    zil.debug_level = 3
+    
+    #  Adjust gas_limit value ...
+    zil.transaction['gas_limit'] = 2000000000
+    zil.transaction['gas_limit'] = 20000
+    
+    print("Deploying contract from file:", filename)
+    
+    balance1 = zil.account.get_balance()
+    print("Balance (before):", balance1)
+    
+    #  Setup the payee account ...
+    '''
+    keystore_file = Config.keystore_file
+    keystore_pwd = Config.keystore_passphrase
+    account = Account.from_keystore(keystore_pwd, keystore_file)
+    '''
+    payee_account = Account(
+                           address="zil1fftsz0t5lvdlhlhj4cal9xzva9cvulmz5tv7sx")
+
+    start_deploy = time.time()
+    print(f"Starting (compile and) deployment at: {time.ctime(start_deploy)}")
+    
+    #  Define the params for the contract ...
+    init_params = [
+        #Contract.value_dict("payee", "ByStr20", zil.account.address0x),
+        Contract.value_dict("payee", "ByStr20", payee_account.address0x),
+        Contract.value_dict("penaltyThreshold", "Uint32", "4"),
+        Contract.value_dict("penaltyAmount", "Uint128", "10"),
+    ]
+    
+    return
+    contract, status = zil.compile_and_deploy(filename, init_params=init_params)
+    end_deploy = time.time()
+    print(f"Ending deployment at: {time.ctime(end_deploy)}  Duration: {end_deploy - start_deploy}s")
+    
+    print(f"Outcome: Status: {status}   Contract: {contract}")
+    
+    receipt = zil.contract.last_receipt
+    
+    if status.value != "Deployed":
+        print(f"Deploy failed '{status}' - receipt:  {receipt}")
+    
+    balance2 = zil.account.get_balance()
+    print("Balance (after):", balance2)
+    
+    print("Zil used (deployment):", balance1 - balance2)
+    
+    end = time.time()
+    
+    print(f"Ending at: {time.ctime(end)}  Duration: {end - start}s")
+
+if __name__ == '__main__':
+    print("%s (%s-%s)" % (__TOOL, __VERSION, __DESC))
+    main(sys.argv)
--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,6 @@
 #******************************************************************************
 #* Filename:            config.py
-#* Last Updated:        2019-09-06
+#* Last Updated:        2019-09-26
 #* Status:              Prototype
 #* Description:         Config data for zil_01 object
 #* Project:             Zilliqa dApp Project
@@ -12,6 +12,7 @@
 
 class Config(object):
     #
+    #'''
     #  Default account definitions ...
     #  ... update this with your account details.
     #default_account_address = '123456789abcdef0123456789abcdef012345668'
@@ -19,5 +20,29 @@ class Config(object):
     keystore_file = 'data/accounts/test_account_1.json'
     keystore_passphrase = 'machine economy passphrase 0001'
     #
+    #  Payee account definitions ...
+    #  ... update this with your account details.
+    #payee_account_address = 'zil123456789012345678901234567890123456789'
+    #payee_account_address = '[Add account address here]'
+    #
+    #
+    #  Default contract defn ...
+    #default_contract_address = 'ccc1234567890123456789012345678901234567'
+    default_contract_address = None
+    '''
+    #
+    # ******      Working Data       *****
+    #
+    default_account_address = 'b50b61ecb98683083ad80f45bde5d15ea5ab745b'
+    keystore_file = 'data/accounts/test_0001_keystore.json'
+    keystore_passphrase = 'test account 0001 passphrase'
+    #
+    #
+    #  Payee account definitions ...
+    #  ... update this with your account details.
+    payee_account_address = 'zil1fftsz0t5lvdlhlhj4cal9xzva9cvulmz5tv7sx'
+    #
+    #
     #  Default contract defn ...
     default_contract_address = None
+    '''
