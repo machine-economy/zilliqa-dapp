a7b83dc2df30d5fafbfa0004794e3cf989c4ad79
Vladimir Komendantskiy <komendantsky@gmail.com>
2019-10-08 19:14:17 +0100

    added a test of initial balance in the mock accounts

--- test/test_mock.py
+++ test/test_mock.py
@@ -2,22 +2,47 @@
 # Copyright (c) 2019  Well Bred Software Limited and Rustiq Technology Limited
 # This code is licensed under the MIT license (see LICENSE for terms).
 
+#  Fix-up the path to the 'root' directory ...
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 import logging
+from pyzil.account import Account
+from pyzil.zilliqa.chain import BlockChain
+from pyzil.zilliqa.units import Qa, Zil
+
 from mock.src.getAccounts import accountPrivateKeys
+from src import zil_01
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
 
+# Interface to the local Kaya instance.
+mockNet = BlockChain("http://localhost:4200/", version = 1, network_id = 111)
+
 # Test the mock accounts.
 def test_mock_accounts():
     # There are 10 initial accounts by default.
-    len(accountPrivateKeys) == 10
-    for acc in accountPrivateKeys:
+    assert len(accountPrivateKeys) == 10
+    for adr in accountPrivateKeys:
         # Every account number is a 20-byte hex string.
-        len(acc) == 40
-        accn = int(acc, 16)
+        assert len(adr) == 40
+        accn = int(adr, 16)
         # Every secret key is a 32-byte hex string.
-        key = accountPrivateKeys[acc]
-        len(key) == 64
+        key = accountPrivateKeys[adr]
+        assert len(key) == 64
         keyn = int(key, 16)
+
+# Test the account API and the initial balances.
+def test_initial_balances():
+    for adr in accountPrivateKeys:
+        account = Account(address = adr, private_key = accountPrivateKeys[adr])
+        zil = zil_01.Zil_01(net = mockNet, account = account)
+        # Check we have an object.
+        assert zil is not None
+        # Check the account field is set.
+        assert zil.account == account
+        # Check the initial balance.
+        assert zil.get_balance() == Qa(10 ** 18).toZil()
